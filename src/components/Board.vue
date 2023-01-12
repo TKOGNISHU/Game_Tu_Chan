94b2aa964bfcdc5d3b84f52796c617813e591ad9
ChiHoa <kyuseishu1593@gmail.com>
2023-01-13 01:17:38 +0700

    covert deg

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -63,19 +63,19 @@
                 </svg> -->
 
                 <svg :width="vx" :height="vy" :viewBox="`0 0 ${vx} ${vy}`" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <!-- Background 1453 875-->
+                    <!-- Background 1453 875    (góc - 30)  -->
                     <path fill-rule="evenodd" clip-rule="evenodd"
-                        :d="`M ${vx - 43.37} 0
-                            L ${vx} 45.1327 V ${vy - 49.547}
-                            L ${vx - 43.25} ${vy} H 44.8116
-                            L 0 ${vy - 49.209} V 45.8729
-                            L 44.0796 0 Z`"
+                        :d="`M ${vx - 13.37} 0
+                            L ${vx} 15.1327 V ${vy - 19.547}
+                            L ${vx - 13.25} ${vy} H 14.8116
+                            L 0 ${vy - 19.209} V 15.8729
+                            L 14.0796 0 Z`"
                         fill="#66BCED" fill-opacity="0.5" />
                     <!-- Top line -->
                     <path fill-rule="evenodd" clip-rule="evenodd"
-                        :d="`M 31.2656 14
+                        :d="`M 1.2656 14
                             H ${vx - 132.79} V 8
-                            H 36.834 Z`"
+                            H 6.834 Z`"
                         fill="#6A87EA" />
                     <path fill-rule="evenodd" clip-rule="evenodd" 
                         :d="`M 21.057 25
@@ -86,7 +86,7 @@
                     <path fill-rule="evenodd" clip-rule="evenodd"
                         :d="`M ${vx - 23.74} ${vy - 21}
                             H 25.6881
-                            L 31.152 ${vy - 15} H ${1423.69 + vx}
+                            L 31.152 ${vy - 15} H ${vx - 29.31}
                             L ${vx - 23.74} ${vy - 21} Z`" fill="#6A87EA" />
                     <path fill-rule="evenodd" clip-rule="evenodd"
                         :d="`M ${vx - 33.03} ${vy - 11}
@@ -98,63 +98,63 @@
                     <rect :x="vx - 21" y="146" width="4" :height="vy - 211" fill="#6A87EA" />
                     <rect :x="vx - 31" y="146" width="4" :height="vy - 211" fill="#6A87EA" />
                     <!-- Group bottom right -->
-                    <rect :x="1389 + vx" :y="806 + vy" width="64" height="4" fill="#6A87EA" />
-                    <rect :x="1389.19 + vx" :y="806 + vy" width="4" height="66" :transform="`rotate(43.2187 ${1389.19 + vx} ${806 + vy})`"
+                    <rect :x="vx - 64" :y="vy - 69" width="64" height="4" fill="#6A87EA" />
+                    <rect :x="vx - 63.81" :y="vy - 69" width="4" height="66" :transform="`rotate(43.2187 ${vx - 63.81} ${vy - 69})`"
                         fill="#6A87EA" />
                     <!-- Group bottom left -->
-                    <rect width="64" height="4" :transform="`matrix(-1 0 0 1 64 ${806 + vy})`" fill="#6A87EA" />
-                    <rect width="4" height="66" :transform="`matrix(-0.728745 0.684785 0.684785 0.728745 63.8151 ${806 + vy})`"
+                    <rect width="64" height="4" :transform="`matrix(-1 0 0 1 64 ${vy - 69})`" fill="#6A87EA" />
+                    <rect width="4" height="66" :transform="`matrix(-0.728745 0.684785 0.684785 0.728745 63.8151 ${vy - 69})`"
                         fill="#6A87EA" />
                     <!-- Group top right -->
                         <!-- 1 -->
-                        <rect :x="1388 + vx" y="87" width="65" height="2" fill="#06A5FE" fill-opacity="0.5" />
+                        <rect :x="vx - 65" y="87" width="65" height="2" fill="#06A5FE" fill-opacity="0.5" />
                         <path
-                            :d="`M ${1355 + vx} 68.2593
-                                L ${1390.42 + vx} 87.8649
-                                V 87.8649 C ${1389.88 + vx} 88.8313 ${1388.67 + vx} 89.181 ${1387.7 + vx} 88.6461
-                                L ${1354.03 + vx} 70.0091
-                                L ${1355 + vx} 68.2593 Z`"
+                            :d="`M ${vx - 98} 68.2593
+                                L ${vx - 62.58} 87.8649
+                                V 87.8649 C ${vx - 63.12} 88.8313 ${vx - 64.33} 89.181 ${vx - 65.3} 88.6461
+                                L ${vx - 98.97} 70.0091
+                                L ${vx - 98} 68.2593 Z`"
                             fill="#06A5FE" fill-opacity="0.5" />
                         <path
-                            :d="`M ${1320.97 + vx} 35
-                                L ${1356.15 + vx} 54.473
-                                L ${1355 + vx}.18 56.2228
-                                L ${1320 + vx} 36.7498
-                                L ${1320.97 + vx} 35 Z`" fill="#06A5FE"
+                            :d="`M ${vx - 132.03} 35
+                                L ${vx - 96.85} 54.473
+                                L ${vx - 98}.18 56.2228
+                                L ${vx - 133} 36.7498
+                                L ${vx - 132.03} 35 Z`" fill="#06A5FE"
                             fill-opacity="0.5" />
-                        <path :d="`M ${1354 + vx} 70 V 54.5 H ${1356  +vx} V70 H ${1354 + vx} Z`" fill="#06A5FE" fill-opacity="0.5" />
+                        <path :d="`M ${vx - 99} 70 V 54.5 H ${vx - 97} V70 H ${vx - 99} Z`" fill="#06A5FE" fill-opacity="0.5" />
                         <!-- 2 -->
-                        <rect :x="1384 + vx" y="90" width="69" height="2" fill="#06A5FE" fill-opacity="0.5" />
+                        <rect :x="vx - 69" y="90" width="69" height="2" fill="#06A5FE" fill-opacity="0.5" />
                         <path
-                            :d="`M ${1351 + vx} 71.2598
-                                L ${1386.42 + vx} 90.865
-                                V 90.865 C ${1385.88 + vx} 91.8314 ${1384.67 + vx} 92.1812 ${1383.7 + vx} 91.6462
-                                L ${1350.03 + vx} 73.0096
-                                L ${1351 + vx} 71.2598 Z`"
+                            :d="`M ${vx - 103} 71.2598
+                                L ${vx - 66.58} 90.865
+                                V 90.865 C ${vx - 67.12} 91.8314 ${vx - 68.33} 92.1812 ${vx - 70} 91.6462
+                                L ${vx - 102.97} 73.0096
+                                L ${vx - 103} 71.2598 Z`"
                             fill="#06A5FE" fill-opacity="0.5" />
                         <path
-                            :d="`M ${1316.32 + vx} 39.9068 C ${1316.89 + vx} 38.971 ${1318.11 + vx} 38.672 ${1319.04 + vx} 39.239
-                                L ${1351 + vx}.82 59.097
-                                L ${1350.79 + vx} 60.7914
-                                L ${1316.32 + vx} 39.9068
+                            :d="`M ${vx - 136.68} 39.9068 C ${vx - 136.11} 38.971 ${vx - 134.89} 38.672 ${vx - 133.96} 39.239
+                                L ${vx - 103}.82 59.097
+                                L ${vx - 102.21} 60.7914
+                                L ${vx - 136.68} 39.9068
                                 V 39.9068 Z`"
                             fill="#06A5FE" fill-opacity="0.5" />
-                        <path :d="`M ${1350  +vx} 73 V 59 H ${1352  +vx} V 73 H ${1350  +vx} Z`" fill="#06A5FE" fill-opacity="0.5" />
+                        <path :d="`M ${vx - 103} 73 V 59 H ${vx - 101} V 73 H ${vx - 103} Z`" fill="#06A5FE" fill-opacity="0.5" />
                         <!-- Vòng tròn -->
                         <path
-                            :d="`M ${1321 + vx} 34.5
-                                C ${1321 + vx}          37.5376         ${1318.54 + vx}         40              ${1315.5 + vx}      40
-                                C ${1312.46 + vx}       40              ${1310 + vx}            37.5376         ${1310 + vx}        34.5
-                                C ${1310 + vx}          31.4624         ${1312.46 + vx}         29              ${1315.5 + vx}      29
-                                C ${1318.54 + vx}       29              ${1321 + vx}            31.4624         ${1321 + vx}        34.5 Z
-                               M ${1310.6 + vx} 34.5
-                                C ${1310.6 + vx}        37.2082         ${1312.79 + vx}         39.4037         ${1315.5 + vx}      39.4037
-                                C ${1318.21 + vx}       39.4037         ${1320.4 + vx}          37.2082         ${1320.4 + vx}      34.5
-                                C ${1320.4 + vx}        31.7918         ${1318.21 + vx}         29.5963         ${1315.5 + vx}      29.5963
-                                C ${1312.79 + vx}       29.5963         ${1310.6 + vx}          31.7918         ${1310.6 + vx}      34.5 Z`"
+                            :d="`M ${vx - 132} 34.5
+                                C ${vx - 132}          37.5376         ${vx - 134.46}         40              ${vx - 137.5}      40
+                                C ${vx - 140.54}       40              ${vx - 143}            37.5376         ${vx - 143}        34.5
+                                C ${vx - 143}          31.4624         ${vx - 140.54}         29              ${vx - 137.5}      29
+                                C ${vx - 134.46}       29              ${vx - 132}            31.4624         ${vx - 132}        34.5 Z
+                               M ${vx - 142.4} 34.5
+                                C ${vx - 142.4}        37.2082         ${vx - 140.21}         39.4037         ${vx - 137.5}      39.4037
+                                C ${vx - 134.79}       39.4037         ${vx - 132.6}          37.2082         ${vx - 132.6}      34.5
+                                C ${vx - 132.6}        31.7918         ${vx - 134.79}         29.5963         ${vx - 137.5}      29.5963
+                                C ${vx - 140.21}       29.5963         ${vx - 142.4}          31.7918         ${vx - 142.4}      34.5 Z`"
                             fill="#06A5FE" fill-opacity="0.5" />
                         <!-- circle top right -->
-                        <circle :cx="1315.5 + vx" cy="34.5" r="1.5" fill="#06A5FE" fill-opacity="0.5" />
+                        <circle :cx="vx - 137.5" cy="34.5" r="1.5" fill="#06A5FE" fill-opacity="0.5" />
                     <!-- ##################### -->
 
                     <!-- Group top left #No-change -->

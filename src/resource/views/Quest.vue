c5989909736742b26ee30afc2c7b2f4f613e2d95
ChiHoa <kyuseishu1593@gmail.com>
2023-01-20 13:07:40 +0700

    design skills plot

--- src/resource/views/Quest.vue
+++ src/resource/views/Quest.vue
@@ -10,6 +10,20 @@
 export default {
     data() {
         return {
+            avatars: [
+                {
+                    first: '@/assets/img/nam-tu-si/nam_tu_si_no_background.png',
+                    second: '@/assets/img/nam-tu-si/nam_tu_si_thi_phap_second.png',
+                    between: '@/assets/img/nam-tu-si/nam_tu_si_no_background_between.png',
+                    finally: '@/assets/img/nam-tu-si/nam_tu_si_thi_phap_finally.png',
+                },
+            ],
+            skills: {
+                'normal': {
+                    first: '@/assets/img/skills/normal/normal_first.png',
+                    finally: '@/assets/img/skills/normal/normal_finally.png',
+                },
+            },
             plot: [ // turn
                 [
                     { // you
@@ -70,13 +84,76 @@ export default {
                         local: 1,
                         beingAttacked: [1], // 1, 2, 3, 4, 5, first row, last row, first column, last column
                         damage: [100],
-                        effect: ['http://localhost...'],
+                        attackedEffect: 'normal',
                         heal: [0],
                         beingHealed: [0], // 0 => nothing, 1 => only, 2 => random, 3 => all
+                        healedEffect: '',
                         // healingNumberOf: 0, // 0 => nothing, > 0 => healing number of
                         preStatuses: [0], // > 0 => pre heal, < 0 => pre damage
                     },
-                    {}, // defense
+                    { // defense
+                        you: [
+                            {
+                                stt: 1,
+                                health: 1000,
+                                mp: 100,
+                            },
+                            {
+                                stt: 2,
+                                health: 1000,
+                                mp: 100,
+                            },
+                            {
+                                stt: 3,
+                                health: 1000,
+                                mp: 100,
+                            },
+                            {
+                                stt: 4,
+                                health: 1000,
+                                mp: 100,
+                            },
+                            {
+                                stt: 5,
+                                health: 1000,
+                                mp: 100,
+                            },
+                        ],
+                        defense: [
+                            {
+                                stt: 1,
+                                health: 1000,
+                                mp: 100,
+                            },
+                            {
+                                stt: 2,
+                                health: 1000,
+                                mp: 100,
+                            },
+                            {
+                                stt: 3,
+                                health: 1000,
+                                mp: 100,
+                            },
+                            {
+                                stt: 4,
+                                health: 1000,
+                                mp: 100,
+                            },
+                            {
+                                stt: 5,
+                                health: 1000,
+                                mp: 100,
+                            },
+                        ],
+                        local: 1,
+                        beingAttacked: [1],
+                        damage: [100],
+                        effect: ['http://localhost...'],
+                        heal: [0],
+                        beingHealed: [0],
+                        preStatuses: [0],
+                    },
                 ]
             ]
         }

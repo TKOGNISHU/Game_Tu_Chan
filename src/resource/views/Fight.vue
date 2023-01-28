51415b240f95e8fb5d8c0ebf7fac82a7d5717a56
ChiHoa <kyuseishu1593@gmail.com>
2023-01-28 17:09:50 +0700

    update skill

--- src/resource/views/Fight.vue
+++ src/resource/views/Fight.vue
@@ -1,31 +1,25 @@
 <template>
     <section class="position-absolute top-0 end-0 start-0 h-100 text-center background">
-        <div class="status-bar" style="height: 100px;"><h1>Status</h1></div>
+        <section class="status-bar" style="height: 150px;"><h1>Status</h1></section>
 
-        <div class="row battle-field">
-            <!-- you -->
+        <section class="row battle-field">
+            <!-- you  -->
             <div class="col bg-dark">
                 <div class="row g-0">
-                    <div v-for="i of [3, 2, 1]" class="col position-relative bg-success" style="height: 140px;">
-                        <img v-if="status.you[i]" :class="`h-100 you-${i}-first`" style="z-index: 100;" :src="avatars[status.you[i].avatar].first" alt="">
-                        <img v-if="status.you[i]" :class="`h-100 d-none you-${i}-second`" style="z-index: 100;" :src="avatars[status.you[i].avatar].second" alt="">
-                        <img v-if="status.you[i]" :class="`h-100 d-none you-${i}-between`" style="z-index: 100;" :src="avatars[status.you[i].avatar].between" alt="">
+                    <div v-for="i of [7, 4, 1]" :class="`col position-relative bg-success ${`you-${i}`}`" style="height: 140px;">
+                        <img v-if="status.you[i]" :class="`d-inline-block h-100 you-${i}-first`" style="z-index: 100;" :src="avatars[status.you[i].avatar].first" alt="">
+                        <img v-if="status.you[i]" :class="`d-inline-block h-100 d-none you-${i}-second`" style="z-index: 100;" :src="avatars[status.you[i].avatar].second" alt="">
+                        <img v-if="status.you[i]" :class="`d-inline-block h-100 d-none you-${i}-between`" style="z-index: 100;" :src="avatars[status.you[i].avatar].between" alt="">
                         <img v-if="status.you[i]" @load="chanting('you', i, 1000)" :class="`h-100 d-none you-${i}-finally`" style="z-index: 100;" :src="avatars[status.you[i].avatar].finally" alt="">
-                        
-                        <div class="position-absolute top-0 bottom-0 start-0" style="margin-top: -100px;">
-                            <img class="h-75" src="@/assets/img/skills/normal/normal_first.png" alt="">
-
-                            <img :class="`position-absolute h-25 you-skill-${i}`" style="top: 22%; left: 36%;" src="@/assets/img/skills/normal/normal_finally.png" alt="">
-                        </div>
                     </div>
                 </div>
                 <div class="row g-0">
-                    <div v-for="i of [6, 5, 4]" class="col position-relative bg-danger" style="height: 140px;">
+                    <div v-for="i of [8, 5, 2]" class="col position-relative bg-danger" style="height: 140px;">
                         <img v-if="status.you[i]" :class="`h-100 you-${i}`" :src="avatars[status.you[i].avatar].first" alt="">
                     </div>
                 </div>
                 <div class="row g-0">
-                    <div v-for="i of [9, 8, 7]" class="col position-relative bg-warning" style="height: 140px;">
+                    <div v-for="i of [9, 6, 3]" class="col position-relative bg-warning" style="height: 140px;">
                         <img v-if="status.you[i]" :class="`h-100 you-${i}`" :src="avatars[status.you[i].avatar].first" alt="">
                     </div>
                 </div>
@@ -34,22 +28,37 @@
             <!-- defense -->
             <div class="col bg-dark">
                 <div class="row g-0">
-                    <div v-for="i of [3, 2, 1]" class="col position-relative bg-success" style="height: 140px;">
-                        <img v-if="status.defense[i]" :class="`h-100 defense-${i}`" style="transform: scaleX(-1);" :src="avatars[status.defense[i].avatar].first" alt="">
+                    <div v-for="i of [1, 4, 7]" :class="`col position-relative bg-success`" style="height: 140px;">
+                        <img v-if="status.defense[i]" :class="`d-inline-block h-100 defense-${i}`" style="transform: scaleX(-1);" :src="avatars[status.defense[i].avatar].first" alt="">
                     </div>
                 </div>
                 <div class="row g-0">
-                    <div v-for="i of [6, 5, 4]" class="col position-relative bg-danger" style="height: 140px;">
-                        <img v-if="status.defense[i]" :class="`h-100 defense-${i}`" style="transform: scaleX(-1);" :src="avatars[status.defense[i].avatar].first" alt="">
+                    <div v-for="i of [2, 5, 8]" class="col position-relative bg-danger" style="height: 140px;">
+                        <img v-if="status.defense[i]" :class="`d-inline-block h-100 defense-${i}`" style="transform: scaleX(-1);" :src="avatars[status.defense[i].avatar].first" alt="">
                     </div>
                 </div>
                 <div class="row g-0">
-                    <div v-for="i of [9, 8, 7]" class="col position-relative bg-warning" style="height: 140px;">
-                        <img v-if="status.defense[i]" :class="`h-100 defense-${i}`" style="transform: scaleX(-1);" :src="avatars[status.defense[i].avatar].first" alt="">
+                    <div v-for="i of [3, 6, 9]" class="col position-relative bg-warning" style="height: 140px;">
+                        <img v-if="status.defense[i]" :class="`d-inline-block h-100 defense-${i}`" style="transform: scaleX(-1);" :src="avatars[status.defense[i].avatar].first" alt="">
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+
+        <!-- Skills -->
+        <section>
+            <template v-for="(value, key, index) in skysState">
+                <img :class="`position-fixed d-inline-block ${key}`" :src="value.image" :style="value.style" alt="">
+            </template>
+            
+            <template v-for="(value, key, index) in magicRings">
+                <img :class="`position-fixed d-inline-block ${key}`" :src="value.image" :style="value.style" alt="">
+            </template>
+
+            <template v-for="(value, key, index) in skills">
+                <img v-for="i in value.amount" :class="`position-fixed d-inline-block skill-${key}`" :src="value.image" :style="value.style" alt="">
+            </template>
+        </section>
     </section>
 </template>
 
@@ -58,6 +67,9 @@ import monkFirst from '@/assets/img/nam-tu-si/nam_tu_si_no_background.png'
 import monkSecond from '@/assets/img/nam-tu-si/nam_tu_si_thi_phap_second.png'
 import monkBetween from '@/assets/img/nam-tu-si/nam_tu_si_no_background_between.png'
 import monkFinally from '@/assets/img/nam-tu-si/nam_tu_si_thi_phap_finally.png'
+
+import magic_ring_normal from '@/assets/img/skills/normal/normal_first.png'
+import normal_finally from '@/assets/img/skills/normal/normal_finally.png'
 
 export default {
     data() {
@@ -72,10 +84,21 @@ export default {
                     finally: monkFinally,
                 },
             },
+            skysState: {
+
+            },
+            magicRings: {
+                magic_ring_normal: {
+                    image: magic_ring_normal,
+                    style: 'width: 150px; margin-top: -150px; margin-left: -20px'
+                },
+            },
             skills: {
                 'normal': {
-                    first: '@/assets/img/skills/normal/normal_first.png',
-                    finally: '@/assets/img/skills/normal/normal_finally.png',
+                    local: 'you', // you - defense - sky
+                    amount: 1, // number of skill attend to defense same time
+                    image: normal_finally,
+                    style: 'width: 150px; z-index: 1000; margin-top: -50px;',
                 },
             },
             status: {
@@ -160,6 +183,12 @@ export default {
             const final = $(`.${who}-${index}-finally`)
 
             timeout(1000).then(async () => {
+                // Display skill
+                if (index == 1) {
+                    this.setLocalFirst('magic_ring_normal', `${who}-${index}`)
+                    this.setLocalFirst('skill-normal', `${who}-${index}`)
+                }
+
                 second.classList.remove('d-none')
                 first.classList.add('d-none')
                 await timeout(30).then(async () => {
@@ -167,9 +196,18 @@ export default {
                     second.classList.add('d-none')
                 })
 
-                this.actionSkill('you-skill-1', 'defense-3')
+                await timeout(500).then(async () => {
+                    if (index == 1) {
+                        this.actionSkill(`${who}-${index}`, 'skill-normal', 'defense-5')
+                    }
+                })
                 await timeout(time).then(async () => {
                     this.chantingFinish(who, index)
+                    // Hidden skill
+                    if (index == 1) {
+                        this.setLocalFirst('magic_ring_normal', `app`)
+                        this.setLocalFirst('skill-normal', `app`)
+                    }
                 })
             })
         },
@@ -193,9 +231,30 @@ export default {
                 })
             })
         },
-        async actionSkill(yourSkill = 'you-skill-1', object = 'defense-3') {
+        setLocalFirst(yourSkill, object) {
             const skill = $(`.${yourSkill}`)
-            skill.style.translate = `${$(`.${object}`).x - $(`.${yourSkill}`).x - $(`.${object}`).offsetWidth - $(`.${yourSkill}`).offsetWidth}px 0px`
+            // Local rect on viewport
+            const skillRect = $(`.${yourSkill}`).getBoundingClientRect()
+            const objectRect = $(`.${object}`).getBoundingClientRect()
+
+            skill.style.top = `${objectRect.y}px`
+            skill.style.left = `${objectRect.x}px`
+        },
+        async actionSkill(who, yourSkill, object) {
+            const skill = $(`.${yourSkill}`)
+            // Local rect on viewport
+            const skillRect = $(`.${yourSkill}`).getBoundingClientRect()
+            const defense = $(`.${object}`).getBoundingClientRect()
+            // 
+            const x =   defense.x
+                      - skillRect.x // => object <=> yourSkill
+                      - skillRect.width / 2
+            // 
+            const y =   defense.y
+                      - skillRect.y // => object <=> yourSkill
+                      + skillRect.height / 2
+
+            skill.style.translate = `${x}px ${y}px`
         }
     },
     mounted() {
